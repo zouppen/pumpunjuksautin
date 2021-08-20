3f1018a196d6b458ae9aa4449c7f53008b5fe1a0
Joel Lehtonen <joel.lehtonen@iki.fi>
2021-08-20 18:38:49 +0300

    avr: Important fix in timer comments The unit is not milliseconds!

--- avr/src/clock.h
+++ avr/src/clock.h
@@ -10,7 +10,6 @@ void clock_set(time_t now, int32_t zone);
 // Test if clock is already set or is it running fake time
 bool clock_is_set(void);
 
-// Arm timer. Timeout given in milliseconds but they have accuracy of
-// prescaler / F_CPU = 16µs and maximum delay of prescaler * COUNT_A /
-// F_CPU = 4ms.
+// Arm timer. Timeout unit is prescaler / F_CPU = 16µs and maximum
+// delay of prescaler * COUNT_A / F_CPU = 4ms.
 void clock_arm_timer(uint8_t timeout);
