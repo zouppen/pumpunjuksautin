1fa1e8e81708fde2b60734343a7de04afab3900f
Joel Lehtonen <joel.lehtonen@iki.fi>
2021-09-06 22:49:53 +0200

    avr: Remove an old comment #10

--- avr/src/main.c
+++ avr/src/main.c
@@ -15,10 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// Note, many macro values are defined in <avr/io.h> and
-// <avr/interrupts.h>, which are included automatically by
-// the Arduino interface
-
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
